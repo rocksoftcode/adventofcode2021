eb54c095f8655654c3dace3172d30ff3c01fe4ea
Rocky DeVries <rocky.devries.contractor@hilton.com>
2021-12-04 15:48:16 -0600

    Day 4

--- day4/bingo-1.groovy
+++ day4/bingo-1.groovy
@@ -5,16 +5,14 @@ List<List<List<Integer>>> boards = []
 int line = 1
 while (line++ < lines.size()) {
 	List<List<Integer>> board = []
-	5.times { board << lines[line++].trim().split(/\s+/)*.toInteger() }
+	5.times {  board << lines[line++].trim().split(/\s+/)*.toInteger() }
 	boards << board
 }
 def addAll = { List<List<Integer>> board ->
 	board.sum { it.sum {Math.max(it, 0) }}
 }
 calls.each {call ->
-	boards.each {board ->
-		board.each {row -> Collections.replaceAll(row, call, -1)}
-	}
+	boards.each { board -> board.each { row -> Collections.replaceAll(row, call, -1)} }
 	List<List<Integer>> rowMatch = boards.find {board -> board.any {row -> row[0] + row[1] + row[2] + row[3] + row[4] == -5 }}
 	List<List<Integer>> colMatch = boards.find {board -> for (col in 0..<5) if (board[0][col] + board[1][col] + board[2][col] + board[3][col] + board[4][col] == -5) return true; return false }
 	if (rowMatch || colMatch) {
--- day4/bingo-2.groovy
+++ day4/bingo-2.groovy
@@ -6,7 +6,7 @@ List<List<List<Integer>>> boards = []
 int line = 1
 while (line++ < lines.size()) {
 	List<List<Integer>> board = []
-	5.times {board << lines[line++].trim().split(/\s+/)*.toInteger() }
+	5.times { board << lines[line++].trim().split(/\s+/)*.toInteger() }
 	boards << board
 }
 
@@ -14,10 +14,7 @@ def addAll = { List<List<Integer>> board ->
 	board.sum { it.sum {Math.max(it, 0) }}
 }
 calls.each {call ->
-	boards.each {board ->
-		board.each {row -> Collections.replaceAll(row, call, -1)}
-	}
-
+	boards.each { board -> board.each { row -> Collections.replaceAll(row, call, -1) } }
 	List<List<List<Integer>>> rowMatch = boards.findAll {board -> board.any {row -> row[0] + row[1] + row[2] + row[3] + row[4] == -5 } }
 	List<List<List<Integer>>> colMatch = boards.findAll {board -> for (col in 0..<5) if (board[0][col] + board[1][col] + board[2][col] + board[3][col] + board[4][col] == -5) return true; return false }
 
