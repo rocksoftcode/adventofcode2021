a68e39c504e5213f03de1464b509003e0182d9f0
Rocky DeVries <rocky.devries.contractor@hilton.com>
2021-12-04 15:22:02 -0600

    Day 4

--- day4/bingo-1.groovy
+++ day4/bingo-1.groovy
@@ -19,17 +19,8 @@ calls.each {call ->
 	boards.each {board ->
 		board.each {row -> Collections.replaceAll(row, call, -1)}
 	}
-	List<List<Integer>> rowMatch = boards.find {board -> board.any {row -> row.sum() == -5}}
-	List<List<Integer>> colMatch = boards.find {board ->
-		for (col in 0..<5) {
-			int sum = 0
-			for (row in 0..<5) {
-				sum += board[row][col]
-			}
-			if (sum == -5) return true
-		}
-		return false
-	}
+	List<List<Integer>> rowMatch = boards.find {board -> board.any {row -> row[0] + row[1] + row[2] + row[3] + row[4] == -5 }}
+	List<List<Integer>> colMatch = boards.find {board -> for (col in 0..<5) if (board[0][col] + board[1][col] + board[2][col] + board[3][col] + board[4][col] == -5) return true; return false }
 	if (rowMatch || colMatch) {
 		println addAll(rowMatch ?: colMatch) * call
 		System.exit 0
