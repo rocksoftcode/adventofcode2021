a2f90052d31e3d3b439c474be6382601de862f1c
Rocky DeVries <rocky.devries.contractor@hilton.com>
2021-12-04 15:16:51 -0600

    Day 4

--- day4/bingo-2.groovy
+++ day4/bingo-2.groovy
@@ -2,6 +2,7 @@ String raw = new File('input.txt').text
 List<String> lines = raw.split('\n')
 List<Integer> calls = lines[0].split(',')*.toInteger()
 List<List<List<Integer>>> boards = []
+
 int line = 1
 while (line++ < lines.size()) {
 	List<List<Integer>> board = []
@@ -12,6 +13,7 @@ while (line++ < lines.size()) {
 	board << lines[line++].trim().split(/\s+/)*.toInteger()
 	boards << board
 }
+
 def addAll = { List<List<Integer>> board ->
 	board.sum { it.sum {Math.max(it, 0) }}
 }
@@ -19,21 +21,16 @@ calls.each {call ->
 	boards.each {board ->
 		board.each {row -> Collections.replaceAll(row, call, -1)}
 	}
-	List<List<Integer>> rowMatch = boards.find {board -> board.any {row -> row.sum() == -5}}
-	List<List<Integer>> colMatch = boards.find {board ->
-		for (col in 0..<5) {
-			int sum = 0
-			for (row in 0..<5) {
-				sum += board[row][col]
-			}
-			if (sum == -5) return true
-		}
-		return false
-	}
+
+	List<List<List<Integer>>> rowMatch = boards.findAll {board -> board.any {row -> row[0] + row[1] + row[2] + row[3] + row[4] == -5 } }
+	List<List<List<Integer>>> colMatch = boards.findAll {board -> for (col in 0..<5) if (board[0][col] + board[1][col] + board[2][col] + board[3][col] + board[4][col] == -5) return true; return false }
+
 	if (boards.size() == 1) {
 		println addAll(boards[0]) * call
 		System.exit 0
 	} else if (rowMatch || colMatch) {
-		boards.remove(rowMatch ?: colMatch)
+		boards.removeAll(rowMatch)
+		boards.removeAll(colMatch)
 	}
 }
+println boards.size()
