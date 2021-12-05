5ba3fd27969160cac8acf7df9d5dca66541e0359
Rocky DeVries <rocky.devries.contractor@hilton.com>
2021-12-05 11:19:25 -0600

    Day 5

---
+++ day5/lines-2.groovy
@@ -0,0 +1,54 @@
+class Line {
+	int x1
+	int y1
+	int x2
+	int y2
+
+	int maxX() {
+		Math.max(x1, x2)
+	}
+
+	int maxY() {
+		Math.max(y1, y2)
+	}
+}
+
+def toString = {int[][] grid ->
+	grid.each {line -> println line.collect {it == 0 ? '.' : it.toString()}.join(' ')}
+	println ""
+}
+List<Line> lines = new File('input.txt').text.split('\n').collect {
+	List<String> parts = it.split(/\s->\s/)
+	List<Integer> line1 = parts[0].split(',')*.toInteger()
+	List<Integer> line2 = parts[1].split(',')*.toInteger()
+	new Line(x1: line1[0], y1: line1[1], x2: line2[0], y2: line2[1])
+}
+int x = lines.max {Math.max(it.x1, it.x2)}.maxX() + 1
+int y = lines.max {Math.max(it.y1, it.y2)}.maxY() + 1
+int[][] grid = new int[y][x]
+lines.each {
+	if (it.x1 == it.x2 || it.y1 == it.y2) {
+		int minY = Math.min(it.y1, it.y2)
+		int maxY = Math.max(it.y1, it.y2)
+		int minX = Math.min(it.x1, it.x2)
+		int maxX = Math.max(it.x1, it.x2)
+		for (i in minY..maxY) {
+			for (j in minX..maxX) {
+				grid[i][j]++
+			}
+		}
+	} else {
+		int i = it.y1
+		int j = it.x1
+		grid[i][j]++
+
+		int incY = it.y1 > it.y2 ? -1 : 1
+		int incX = it.x1 > it.x2 ? -1 : 1
+		while (i != it.y2 && j != it.x2) {
+			i += incY
+			j += incX
+			grid[i][j]++
+		}
+	}
+}
+println grid.collect().sum {it.collect().count {it > 1}}
