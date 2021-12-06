fe3f5d7e73ffaa4622253e2e6c70d15ed86eab91
Rocky DeVries <rocky.devries.contractor@hilton.com>
2021-12-06 11:40:05 -0600

    Day 6

--- day6/fish-2.groovy
+++ day6/fish-2.groovy
@@ -3,9 +3,7 @@ List<Map<Integer, Long>> stages = [fish.collectEntries { [it, fish.count(it) as 
 (0..8).each {  if (!stages[0][it]) stages[0][it] = 0L }
 (1..256).each {
 	Map<Integer, Long> stage = [8:0]
-	(0..<8).each { day ->
-		stage[day] = stages[it-1][day+1]
-	}
+	(0..<8).each { day -> stage[day] = stages[it-1][day+1] }
 	stage[6] += stages[it-1][0]
 	stage[8] += stages[it-1][0]
 	stages << stage
