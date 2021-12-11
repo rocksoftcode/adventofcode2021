30ee5b992594f2fdc04afa91513a6fec18607f05
rocksoftcode <rocky@rocksoftcode.com>
2021-12-11 11:16:31 -0600

    Day 11

---
+++ day11/octopus-1.groovy
@@ -0,0 +1,24 @@
+Integer[][] grid = new File('input.txt').text.split('\n').collect {it.split('')*.toInteger()}
+long flashes = 0
+def neighbors = {i, j -> [[i - 1, j - 1], [i - 1, j], [i - 1, j + 1], [i, j + 1], [i + 1, j + 1], [i + 1, j], [i + 1, j - 1], [i, j - 1]]}
+def flash
+def inc
+flash = { i, j, flashed ->
+	flashes++
+	flashed[i][j] = true
+	grid[i][j] = 0
+	neighbors(i, j).each { inc(it[0], it[1], flashed) }
+}
+inc = {i, j, flashed ->
+	if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length) {
+		if (grid[i][j] == 9) flash(i, j, flashed)
+		else if (!flashed[i][j]) grid[i][j]++
+	}
+}
+100.times {
+	Boolean[][] flashed = new Boolean[10][10]
+	grid.eachWithIndex { line, i ->
+		line.eachWithIndex { o, j -> inc(i, j, flashed)}
+	}
+}
+println flashes
