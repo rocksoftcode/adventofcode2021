54b0b0125c743fab267d9b9b1dcfb37d3c20f5f5
rocksoftcode <rocky@rocksoftcode.com>
2021-12-12 11:06:28 -0600

    Day 12

---
+++ day12/directions-2.groovy
@@ -0,0 +1,23 @@
+Map<String, List<String>> links = [:]
+new File('input.txt').text.split('\n').each {
+	List<String> parts = it.split('-')
+	if (!links[parts[0]]) links[parts[0]] = [parts[1]]
+	else links[parts[0]] << parts[1]
+	if (!links[parts[1]]) links[parts[1]] = [parts[0]]
+	else links[parts[1]] << parts[0]
+}
+def nav
+def dirs = []
+nav = { node, path, m ->
+	List<String> curr = path.collect()
+	curr << node
+	if (node == 'end') dirs << curr
+	else {
+		for (link in links[node]) {
+			if (link.toUpperCase() == link || !curr.contains(link)) nav(link, curr, m)
+			else if (m && !['start','end'].contains(link)) nav(link, curr, false)
+		}
+	}
+}
+nav('start', [], true)
+println dirs.size()
