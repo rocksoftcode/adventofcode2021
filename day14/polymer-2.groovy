953a4be9aea564efa9070f7adf6bfbaf0f2f6427
rocksoftcode <rocky@rocksoftcode.com>
2021-12-14 11:53:17 -0600

    Day 14

--- day14/polymer-2.groovy
+++ day14/polymer-2.groovy
@@ -1,7 +1,7 @@
 List<String> parts = new File('input.txt').text.split(/\n\n/)
 Map<String, String> rules = parts[1].split('\n').collectEntries {def p = it.split(' -> '); [(p[0]): p[1]]}
 String template = parts[0]
-Map<String, Long> countLetters = [(template[0].toString()): 1L]
+Map<String, Long> countLetters = [(template[0]): 1L]
 Map<String, Long> countPairs = [:]
 for (i in 1..<template.length()) {
 	countPairs[template[i - 1] + template[i]] = 1L + (countPairs[template[i - 1] + template[i]] ?: 0L)
@@ -12,7 +12,7 @@ for (i in 1..<template.length()) {
 	rules.each {
 		if (countPairs[it.key]) {
 			String insert = it.value
-			String pairLeft = it.key[0].toString() + insert
+			String pairLeft = it.key[0] + insert
 			String pairRight = insert + it.key[1]
 			Long count = countPairs[it.key]
 			countPairs[it.key] = 0L
