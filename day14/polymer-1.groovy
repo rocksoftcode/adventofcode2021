d18a356c4277d275ea4d85f611882f2afd125e89
rocksoftcode <rocky@rocksoftcode.com>
2021-12-14 11:41:26 -0600

    Day 14

--- day14/polymer-1.groovy
+++ day14/polymer-1.groovy
@@ -1,17 +1,19 @@
 List<String> parts = new File('input.txt').text.split(/\n\n/)
 String template = parts[0]
-Map<String, String> rules = parts[1].split('\n').collectEntries { def p = it.split(' -> '); [(p[0]): p[1]] }
-def replace = { polymer ->
+Map<String, String> rules = parts[1].split('\n').collectEntries {def p = it.split(' -> '); [(p[0]): p[1]]}
+def replace = {polymer ->
 	Queue<String> elements = polymer.toCharArray()*.toString() as Queue
 	StringBuilder out = new StringBuilder()
 	while (elements) {
 		String c1 = elements.remove()
 		out << c1
-		if (elements) {String c2 = elements.peek(); if (rules["$c1$c2"]) out << rules["$c1$c2"] }
+		if (elements) {
+			String c2 = elements.peek(); if (rules["$c1$c2"]) out << rules["$c1$c2"]
+		}
 	}
 	out
 }
 String polymer = template
-10.times {polymer = replace(polymer) }
-Set<Integer> counts = polymer.toCharArray()*.toString().groupBy {polymer.count(it) }.keySet()
+10.times {polymer = replace(polymer)}
+Set<Integer> counts = polymer.toCharArray()*.toString().groupBy {polymer.count(it)}.keySet()
 println counts.max() - counts.min()
--- day14/polymer-2.groovy
+++ day14/polymer-2.groovy
@@ -1,10 +1,10 @@
 List<String> parts = new File('input.txt').text.split(/\n\n/)
-Map<String, String> rules = parts[1].split('\n').collectEntries { def p = it.split(' -> '); [(p[0]): p[1]] }
+Map<String, String> rules = parts[1].split('\n').collectEntries {def p = it.split(' -> '); [(p[0]): p[1]]}
 String template = parts[0]
-Map<String, Long> countLetters = [(template[0].toString()):1L]
+Map<String, Long> countLetters = [(template[0].toString()): 1L]
 Map<String, Long> countPairs = [:]
 for (i in 1..<template.length()) {
-	countPairs[template[i-1]+template[i]] = 1L + (countPairs[template[i-1]+template[i]] ?: 0L)
+	countPairs[template[i - 1] + template[i]] = 1L + (countPairs[template[i - 1] + template[i]] ?: 0L)
 	countLetters[template[i]] = 1L + (countLetters[template[i]] ?: 0L)
 }
 40.times {
@@ -17,9 +17,9 @@ for (i in 1..<template.length()) {
 			Long count = countPairs[it.key]
 			countPairs[it.key] = 0L
 			countLetters[insert] = count + (countLetters[insert] ?: 0L)
-			[pairLeft,pairRight].each{ newPairCount[it] = count + (newPairCount[it] ?: 0L) }
+			[pairLeft, pairRight].each {newPairCount[it] = count + (newPairCount[it] ?: 0L)}
 		}
 	}
-	newPairCount.each {countPairs[it.key] = it.value + (countPairs[it.key] ?: 0) }
+	newPairCount.each {countPairs[it.key] = it.value + (countPairs[it.key] ?: 0)}
 }
-println countLetters.max { it.value }.value - countLetters.min { it.value }.value
+println countLetters.max {it.value}.value - countLetters.min {it.value}.value
