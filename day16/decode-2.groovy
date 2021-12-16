fb08b91dd2baeee754f1179cd82feadc5c2eadec
rocksoftcode <rocky@rocksoftcode.com>
2021-12-16 10:48:48 -0600

    Day 16

--- day16/decode-2.groovy
+++ day16/decode-2.groovy
@@ -21,24 +21,24 @@ calculate =  { bits ->
 			String part = bits.substring(pos, pos + 5)
 			if (part.startsWith("0")) last = true
 			data.append(part.substring(1))
-			pos += 5;
+			pos += 5
 		}
 		return Long.parseLong(data.toString(), 2)
 	} else {
 		int lengthTypeId = Integer.parseInt(bits.substring(pos, pos + 1), 2)
 		int l = lengthTypeId == 0 ? 15 : 11
-		int length = Integer.parseInt(bits.substring(pos + 1, pos + 1 + l), 2);
-		pos += l + 1;
-		List<Long> results = new ArrayList<>();
+		int length = Integer.parseInt(bits.substring(pos + 1, pos + 1 + l), 2)
+		pos += l + 1
+		List<Long> results = new ArrayList<>()
 		if (lengthTypeId == 0) {
 			while (length > 0) {
 				int start = pos
-				results.add(calculate(bits));
-				length -= (pos - start);
+				results.add(calculate(bits))
+				length -= (pos - start)
 			}
 		} else {
 			for (int i = 0; i < length; i++) {
-				results.add(calculate(bits));
+				results.add(calculate(bits))
 			}
 		}
 		return results.inject({a, b -> operations[typeId](a, b) })
@@ -46,12 +46,12 @@ calculate =  { bits ->
 }
 
 String input = new File('input.txt').text
-StringBuilder builder = new StringBuilder(new BigInteger(input, 16).toString(2));
+StringBuilder builder = new StringBuilder(new BigInteger(input, 16).toString(2))
 if (input.startsWith("0")) {
-	builder.insert(0, "0000");
+	builder.insert(0, "0000")
 }
 if (builder.length() % 4 != 0) {
-	int add = (builder.length() / 4 + 1) * 4 - builder.length();
-	for (int i = 0; i < add; i++) builder.insert(0, "0");
+	int add = (builder.length() / 4 + 1) * 4 - builder.length()
+	for (int i = 0; i < add; i++) builder.insert(0, "0")
 }
-println calculate(builder.toString());
+println calculate(builder.toString())
