dd872500851ff8208b12e483da79533cc9294fd7
rocksoftcode <rocky@rocksoftcode.com>
2021-12-16 10:44:28 -0600

    Day 16

---
+++ day16/decode-1.groovy
@@ -0,0 +1,49 @@
+int versionSum = 0
+int pos = 0
+def calculate
+calculate = { bits ->
+	int version = Integer.parseInt(bits.substring(pos, pos + 3), 2)
+	int typeId = Integer.parseInt(bits.substring(pos + 3, pos + 6), 2)
+	versionSum += version
+	pos += 6
+	if (typeId == 4) {
+		boolean last = false
+		StringBuilder data = new StringBuilder()
+		while (!last) {
+			String part = bits.substring(pos, pos + 5)
+			if (part.startsWith("0")) last = true
+			data << part.substring(1)
+			pos += 5
+		}
+		return Long.parseLong(data.toString(), 2)
+	} else {
+		int lengthTypeId = Integer.parseInt(bits.substring(pos, pos + 1), 2)
+		int l = lengthTypeId == 0 ? 15 : 11
+		int length = Integer.parseInt(bits.substring(pos + 1, pos + 1 + l), 2)
+		pos += l + 1
+		List<Long> results = new ArrayList<>()
+		if (lengthTypeId == 0) {
+			while (length > 0) {
+				int start = pos
+				results.add(calculate(bits))
+				length -= (pos - start)
+			}
+		} else {
+			for (int i = 0; i < length; i++) {
+				results.add(calculate(bits))
+			}
+		}
+		return results
+	}
+}
+String input = new File('input.txt').text
+StringBuilder builder = new StringBuilder(new BigInteger(input, 16).toString(2))
+if (input.startsWith("0")) {
+	builder.insert(0, "0000")
+}
+if (builder.length() % 4 != 0) {
+	int add = (builder.length() / 4 + 1) * 4 - builder.length()
+	for (int i = 0; i < add; i++) builder.insert(0, "0")
+}
+calculate(builder.toString())
+println versionSum
---
+++ day16/decode-2.groovy
@@ -0,0 +1,57 @@
+List operations = [
+		Long::sum,
+		{ a, b -> a * b },
+		{ a, b -> a < b ? a : b },
+		{ a, b -> a > b ? a : b },
+		{ a, b -> null },
+		{ a, b -> a > b ? 1L : 0L },
+		{ a, b -> a < b ? 1L : 0L },
+		{ a, b -> a == b ? 1L : 0L },
+]
+int pos = 0
+
+def calculate
+calculate =  { bits ->
+	int typeId = Integer.parseInt(bits.substring(pos + 3, pos + 6), 2)
+	pos += 6
+	if (typeId == 4) {
+		boolean last = false
+		StringBuilder data = new StringBuilder()
+		while (!last) {
+			String part = bits.substring(pos, pos + 5)
+			if (part.startsWith("0")) last = true
+			data.append(part.substring(1))
+			pos += 5;
+		}
+		return Long.parseLong(data.toString(), 2)
+	} else {
+		int lengthTypeId = Integer.parseInt(bits.substring(pos, pos + 1), 2)
+		int l = lengthTypeId == 0 ? 15 : 11
+		int length = Integer.parseInt(bits.substring(pos + 1, pos + 1 + l), 2);
+		pos += l + 1;
+		List<Long> results = new ArrayList<>();
+		if (lengthTypeId == 0) {
+			while (length > 0) {
+				int start = pos
+				results.add(calculate(bits));
+				length -= (pos - start);
+			}
+		} else {
+			for (int i = 0; i < length; i++) {
+				results.add(calculate(bits));
+			}
+		}
+		return results.inject({a, b -> operations[typeId](a, b) })
+	}
+}
+
+String input = new File('input.txt').text
+StringBuilder builder = new StringBuilder(new BigInteger(input, 16).toString(2));
+if (input.startsWith("0")) {
+	builder.insert(0, "0000");
+}
+if (builder.length() % 4 != 0) {
+	int add = (builder.length() / 4 + 1) * 4 - builder.length();
+	for (int i = 0; i < add; i++) builder.insert(0, "0");
+}
+println calculate(builder.toString());
